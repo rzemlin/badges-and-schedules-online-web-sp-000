d5627c1dc5af3120f5372b04a738a10e7aa31566
Ryan Zemlin <rzemlin@gmail.com>
2021-02-12 14:00:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ end
 def assign_rooms(speakers)
   new_array = []
   speakers.each_with_index do |name, i|
-  new_array << "Hi #{name}! You'll be assigned to room #{i + 1}!"
+  new_array << "Hello #{name}! You'll be assigned to room #{i + 1}!"
   end
 new_array
 end
